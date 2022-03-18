2d933704ce679c7953d623434bfe09bdbb818596
Dez Glidden <dezglidden@gmail.com>
2022-03-18 12:32:05 +0000

    Add initial version file for Moodle 3.11 onwards

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for the local examples plugin
+ *
+ * @package   local_examples
+ * @copyright 2022 onwards Dez Glidden
+ * @author    Dez Glideen <dezglidden@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_examples';
+$plugin->version = 2022031800;
+$plugin->requires = 2021051700; // Moodle 3.11.
+$plugin->release = '0.1.0';
+
