c176f2cd0d3e064e58d04f632a063784c262622d
Dez Glidden <dezglidden@gmail.com>
2022-03-23 12:06:06 +0000

    Add basic dropdown example
    Moodle doesn't have a core template setup to produce a dropdown so
    this commit shows what's involved in producing a Moodle compatible
    dropdown.
    
    There is an odd side effect in the UI when the button is the sole
    element on the page, inside the main content region. The dropdown
    element fails to overflow the region, capping it's appearance. This
    of course is unlikely to be an issue in production since there is
    going to be more than a single button in the content. I have
    resisted the temptation to add custom CSS but adding a min height
    to the aria-label Content selector would fix this.

---
+++ classes/output/dropdown.php
@@ -0,0 +1,81 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_examples\output;
+
+use \renderable;
+use \renderer_base;
+use \stdClass;
+use \templatable;
+
+/**
+ * Class dropdown.
+ *
+ * @package   local_examples
+ * @category  output
+ * @copyright 2022 onwards Dez Glidden
+ * @author    Dez Glideen <dezglidden@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class dropdown implements templatable, renderable {
+
+    /**
+     * Prepare the data for being passed to the renderer.
+     *
+     * @param renderer_base $output
+     * @return stdClass
+     */
+    public function export_for_template(renderer_base $output): stdClass {
+        $data = new stdClass();
+        $data->dropdown = $this->sample();
+        return $data;
+    }
+
+    /**
+     * Produces quick and easy data for displaying a simple dropdown.
+     *
+     * @return array
+     */
+    protected function sample(): array {
+        return [
+            'classes' => 'some things',
+            'trigger' => [
+                'classes' => 'some classes',
+                'attributes' => [
+                    [
+                        'name' => 'data-preference',
+                        'value' => 'prefer'
+                    ]
+                ],
+                    'text' => 'Button text'
+            ],
+            'menu' => [
+                'classes' => 'menu-class',
+                'attributes' => [
+                    [
+                        'name' => 'data-key',
+                        'value' => 'menu'
+                    ]
+                ],
+                'items' => [
+                    ['hasdivider' => false, 'text' => 'First button'],
+                    ['hasdivider' => false, 'text' => 'Second button'],
+                    ['hasdivider' => true, 'text' => 'Third button'],
+                ]
+            ]
+        ];
+    }
+}
